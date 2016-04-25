00c05819def14093ba537e89abc7ff58e3d3ffbf
vasi05 <croitoruvs@gmail.com>
2016-04-25 22:36:41 +0300

    Complete the model

---
+++ BrowseOverflow/Answer.h
@@ -0,0 +1,13 @@
+//
+//  Answer.h
+//  BrowseOverflow
+//
+//  Created by Vasile Croitoru on 25/04/16.
+//  Copyright © 2016 Vasile Croitoru. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Answer : NSObject
+
+@end
---
+++ BrowseOverflow/Answer.m
@@ -0,0 +1,13 @@
+//
+//  Answer.m
+//  BrowseOverflow
+//
+//  Created by Vasile Croitoru on 25/04/16.
+//  Copyright © 2016 Vasile Croitoru. All rights reserved.
+//
+
+#import "Answer.h"
+
+@implementation Answer
+
+@end
